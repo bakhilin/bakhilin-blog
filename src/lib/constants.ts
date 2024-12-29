bdaaaad9dd37f05759651b148c87a26433bd57d4
Bakhilin Nikita <bahilinnikita04@mail.ru>
2024-12-30 00:28:09 +0300

    finish

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+export const LINKS = {
+  github: "https://github.com/bakhilin",
+  email: "bahilinnikita04@mail.ru",
+  telegram: "https://t.me/bakhilin",
+  vk: "https://vk.com/bakhilin"
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
